51bb783350bcd941ffa77ca94eba8fcd8a31593a
Ahmad Ma'sum <ahmadmasum127@gmail.com>
2025-07-29 10:29:11 +0700

    nambah

---
+++ lib/ui/screen/searc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class HalloPage extends StatelessWidget {
+  const HalloPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Hallo Page'));
+  }
+}
